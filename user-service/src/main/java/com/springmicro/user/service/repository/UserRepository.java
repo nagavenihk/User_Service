b4d7bc322666fe7eb310c28c0a3f419a31d65e75
chethan HN <nagavenihk1999@gmail.com>
2024-04-30 18:02:18 +0530

    first Commit

---
+++ user-service/src/main/java/com/springmicro/user/service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springmicro.user.service.repository;
+
+import com.springmicro.user.service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+}
